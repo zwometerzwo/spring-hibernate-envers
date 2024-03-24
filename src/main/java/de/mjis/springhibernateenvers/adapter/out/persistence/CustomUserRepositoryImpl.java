9a7215e102bf73799e9ed9aee7e1e2db742f04e6
Matthias Jahn <jahn.matthias@web.de>
2024-03-24 17:41:05 +0100

    Add minor refactoring

--- src/main/java/de/mjis/springhibernateenvers/adapter/out/persistence/CustomUserRepositoryImpl.java
+++ src/main/java/de/mjis/springhibernateenvers/adapter/out/persistence/CustomUserRepositoryImpl.java
@@ -2,6 +2,7 @@ package de.mjis.springhibernateenvers.adapter.out.persistence;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.hibernate.envers.AuditReader;
 import org.hibernate.envers.AuditReaderFactory;
 
 import java.util.List;
@@ -13,8 +14,9 @@ public class CustomUserRepositoryImpl implements CustomUserRepository {
 
     @Override
     public UserJpaEntity previousRevision(Long id) {
+        AuditReader auditReader = AuditReaderFactory.get(entityManager);
 
-        List<Number> revisions = AuditReaderFactory.get(entityManager).getRevisions(UserJpaEntity.class, 1L);
+        List<Number> revisions = auditReader.getRevisions(UserJpaEntity.class, 1L);
 
         UserJpaEntity auditedUser = (UserJpaEntity) AuditReaderFactory
                 .get(entityManager)
